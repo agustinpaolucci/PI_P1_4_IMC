1ee74c4e7c95fac8deea0af78bb0c654027a7e87
agustinpaolucci <globalprint@gmail.com>
2023-05-08 00:28:35 -0300

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_P1_4_IMC
+{
+    class Persona
+    {
+		
+		// ATRIBUTOS PRIVADOS
+		private string nombre;
+        private int edad;
+        private string sexo;
+        private double peso;
+        private double altura;
+
+
+        // METODOS CONSTRUCTORES 
+		// SOBRE ESCRITURA DEL CONSTRUCTOR SIN PARAMETROS QUE VIENE POR DEFECTO.
+        public Persona()
+        {
+			nombre = string.Empty;
+			edad = 0;
+			sexo = string.Empty;
+			peso = 0.00;
+			altura = 0.00;
+        }
+
+		public Persona(string nombre, double peso, double altura)
+		{
+			this.nombre = nombre;
+			this.peso = peso;
+			this.altura = altura;
+		}
+
+		public Persona(string nombre, int edad, string sexo, double peso, double altura)
+		{
+			this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+
+        // PROPIEDADES PÚBLICAS
+        public string pNombre
+		{
+			get { return nombre; }
+			set { nombre = value; }
+		}
+
+		public int pEdad
+		{
+			get { return edad; }
+			set { edad = value; }
+		}
+
+		public string pSexo
+		{
+			get { return sexo; }
+			set { sexo = value; }
+		}
+
+		public double pPeso
+		{
+			get { return peso; }
+			set { peso = value; }
+		}
+
+		public double pAltura
+		{
+			get { return altura; }
+			set { altura = value; }
+		}
+
+	
+
+		// METODOS DE CONTROL
+
+		// METODO TOSTRING() para RETORNAR UNA CADENA DE CARACTERES QUE REPRESENTAN EL OBJETO
+		public override string ToString()
+		{
+			return "|Nombre: " + nombre + " |Edad: " + edad + " |Sexo: " + sexo + " |Peso: " + peso + " |Altura: " + altura;
+				
+		
+		}
+
+
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using PI_P1_4_IMC;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Persona p01;
+        p01 = new Persona();
+
+        p01.pNombre = "Agustin";
+
+        Persona p02 = new Persona("Juan",24,"H",72.5,1.74);
+
+        Persona p03 = new Persona();
+
+        Console.WriteLine("Escriba el nombre de la persona...");
+        p03.pNombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese la edad de la persona...");
+        p03.pEdad = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(p01.ToString());
+        Console.WriteLine(p02.ToString());
+        Console.WriteLine(p03.ToString());
+
+
+
+
+        Console.WriteLine("Presione una tecla para finalizar");
+        Console.ReadLine();
+        
+
+
+    }
+}
+
+/*
+Problema 1.4:
+Persona. Se nos pide modelar una clase Persona con las siguientes
+condiciones:
+- Sus atributos son: nombre, edad, sexo (H hombre, M mujer), peso y altura.
+- Los métodos que se implementarán son:
+1) calcularIMC(): calculará si la persona está en su peso ideal (peso en
+kg/(altura^2 en m)), si esta fórmula devuelve un valor menor que 20, la
+función devuelve un -1, si devuelve un número entre 20 y 25 (incluidos),
+significa que está por debajo de su peso ideal la función devuelve un 0
+y si devuelve un valor mayor que 25 significa que tiene sobrepeso, la
+función devuelve un 1. Se recomienda usar constantes para devolver
+estos valores.
+2) o esMayorDeEdad(): indica si es mayor de edad(21 años), devuelve un
+booleano.
+ */
